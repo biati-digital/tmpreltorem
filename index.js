1d0816c160cca8654415e845e073fc309393419c
biati-digital <manuel@biati.digital>
2021-10-10 03:53:40 +0000

    Updated

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
+function apolo (r) {
+    console.log(r);
+}
+
 module.exports = (opts = {}) => {
     console.log('process 1');
     console.log('unwanted line from pull request 4');
+    console.log("applesfera");
 };
