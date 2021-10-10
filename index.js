cf9574aaac4dcf040378b58c5ede7db8dc83322d
biati-digital <manuel@biati.digital>
2021-10-10 03:38:02 +0000

    Another linter test

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 module.exports = (opts = {}) => {
     console.log('process 1');
-  console.log('unwanted line from pull request 4');
+  console.log('unwanted line from pull request 5');
+  console.log("this should throw an error");
 };
