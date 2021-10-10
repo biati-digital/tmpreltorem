f1880cfdd70f1cce9c0c3b4cca3004bc9732b330
biati-digital <manuel@biati.digital>
2021-10-10 03:52:12 +0000

    Fixed

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 module.exports = (opts = {}) => {
     console.log('process 1');
-  console.log('unwanted line from pull request 5');
-  console.log("this should throw an error");
+    console.log('unwanted line from pull request 4');
 };
