0eb4aa8fd168b7759302c77bbd270940697eb296
biati-digital <manuel@biati.digital>
2021-10-10 04:59:23 +0000

    Release v1.1.0-beta.1

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 module.exports = (opts = {}) => {
     console.log('process 1');
     console.log('unwanted line from main');
-    console.log('we are done now');
+    console.log('we are done now s');
 };
