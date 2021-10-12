6a1b9b8a7cd06f73edc154a86a490dd938f3faa5
biati-digital <manuel@biati.digital>
2021-10-12 09:42:20 +0000

    Added onepo

--- src/js/glightbox.js
+++ src/js/glightbox.js
@@ -142,6 +142,7 @@ export class GLightbox {
         console.log('and also do another process yes');
         console.log('Rammstein');
         console.log('Supercool');
+        console.log('onepo');
     }
 
     init() {
